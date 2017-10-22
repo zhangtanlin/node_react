13ee043869f1e196fbd1c0c0680aeb175d1193b4
Administrator <zhangtanlin90@126.com>
2017-10-22 22:02:35 +0800

    update首页新闻列表和图片列表

---
+++ 4project/src/js/components/pc_news_block.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {Card} from "antd";
+import {Router,Route,Link,browserHistory} from "react-router";
+
+export default class PCNewsBlock extends React.Component{
+
+  //函数构造
+  constructor(){
+    //调用根函数
+    super();
+    this.state = {
+      news:""//所有新闻列表
+    }
+  };
+
+  //使用生命周期，组件加载前
+  componentWillMount(){
+    //定义Fetch的一些参数【注意是GET】
+    var myFetchOptions = {
+      method:"GET"
+    };
+    /*调用Fetch方法(top:表示,count表示展示的条数。最后返回的是新闻列表数据)
+      注意：这里的type和count是继承自父模块
+    */
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type="+this.props.type+"&count="+this.props.count,myFetchOptions)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          news: json
+        })
+      });
+  };
+
+  render(){
+
+    //定义和判定获取新闻的数据条数
+    const {news} = this.state;
+    const newsList = news.length
+      ?
+      //es6的循环数据的函数方法
+      news.map((newsItem,index) => (
+        <li key={index}>
+          <Link to={`details/${newsItem.uniquekey}`} target="_black">
+            {newsItem.title}
+          </Link>
+        </li>
+      ))
+      :
+      "没有加载到任何新闻"
+    ;
+
+    return(
+      <div class="topNewsList">
+        <Card>
+          <ul>
+            {/*调用列表数据*/}
+            {newsList}
+          </ul>
+        </Card>
+      </div>
+    );
+  };
+}
+
--- 4project/src/js/components/pc_news_container.js
+++ 4project/src/js/components/pc_news_container.js
@@ -1,10 +1,15 @@
 import React from "react";
 //引入antd的：栅格布局、标签页、轮播图、
 import {Row,Col,Tabs,Carousel} from "antd";
-
 const TabPane = Tabs.TabPane;
 
-export default class PCnewsContainer extends React.Component{
+//导入新闻列表模块【动态模块】
+import PCNewsBlock from "./pc_news_block";
+
+//导入新闻图片模块
+import PCNewsImageBlock from "./pc_news_image_block";
+
+export default class PCNewsContainer extends React.Component{
   render(){
 
     {/*定义settings设置*/}
@@ -18,21 +23,42 @@ export default class PCnewsContainer extends React.Component{
 
     return (
       <div>
-        <Col span={2}></Col>
-        <Col span={20} class="container">
-          <div class="leftContainer">
-            <div class="carousel">
-              {/*Carousel有一些settings色设置引入*/}
-              <Carousel {...settings}>
-                {/*注意这里的图片路径是绝对路径【从src开始】*/}
-                <div><img src="./src/images/carousel_1.jpg" alt="1"/></div>
-                <div><img src="./src/images/carousel_2.jpg" alt="2"/></div>
-                <div><img src="./src/images/carousel_3.jpg" alt="3"/></div>
-              </Carousel>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20} class="container">
+            {/*加载轮播*/}
+            <div class="leftContainer">
+              <div class="carousel">
+                {/*Carousel模块有一些settings色设置引入*/}
+                <Carousel {...settings}>
+                  {/*注意这里的图片路径是绝对路径【从src开始】*/}
+                  <div><img src="./src/images/carousel_1.jpg" alt="1"/></div>
+                  <div><img src="./src/images/carousel_2.jpg" alt="2"/></div>
+                  <div><img src="./src/images/carousel_3.jpg" alt="3"/></div>
+                </Carousel>
+              </div>
+
+              {/*加载新闻图片模块【注意：参数和多次调用pc_news_image_block.js】*/}
+              <PCNewsImageBlock count={6} type={"guoji"} cartTitle={"国际头条"} imageWidth={"112px"}></PCNewsImageBlock>
+              <PCNewsImageBlock count={3} type={"yule"} cartTitle={"娱乐头条"} imageWidth={"112px"}></PCNewsImageBlock>
+
             </div>
-          </div>
-        </Col>
-        <Col span={2}></Col>
+            {/*加载新闻列表【通过TabPane多次调用pc_news_block模块】*/}
+            <div class="tabs_news">
+              <Tabs>
+                <TabPane tab="头条新闻" key="1">
+                  {/*type表示新闻类型，top表示头条【请求地址的参数】；count表示新闻条数。
+                注意：{10}和"10"这两种写法，都能定义参数【子模块都能接收得到】*/}
+                  <PCNewsBlock type="top" count={23} border={false}></PCNewsBlock>
+                </TabPane>
+                <TabPane tab="国际" key="2">
+                  <PCNewsBlock type="guoji" count={23} border={false}></PCNewsBlock>
+                </TabPane>
+              </Tabs>
+            </div>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
       </div>
     )
   }
