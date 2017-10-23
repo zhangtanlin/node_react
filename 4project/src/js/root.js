51d714cbff0c4b43e53f5f6f212a7b2bd013119b
zhangtanlin90@126.com <ztl900211>
2017-10-23 17:37:55 +0800

    手机端路由构建以及添加新闻和首页路由

--- 4project/src/js/root.js
+++ 4project/src/js/root.js
@@ -7,11 +7,13 @@ import "antd/dist/antd.css";
 //导入PC端模块
 import PCIndex from "./components/pc_index";
 //导入PC端的新闻详情模块
-import PCNewsDetails from "./components/pc_news_details"
+import PCNewsDetails from "./components/pc_news_details";
 
 
 //导入mobile模块
-import MobileIndex from "./components/mobile_index"
+import MobileIndex from "./components/mobile_index";
+//导入mobile端新闻详情页
+import MobileNewsDetails from "./components/mobile_news_details";
 
 //引入响应式的react布局的包
 import MediaQuery from "react-responsive";
@@ -32,7 +34,13 @@ export default class Root extends React.Component{
         </MediaQuery>
         {/*屏幕最大宽度1224px时，调用*/}
         <MediaQuery query="(max-device-width:1224px)">
-          <MobileIndex/>
+
+          {/*手机端路由重构【和PC端差不多】*/}
+          <Router history={hashHistory}>
+            <Route path="/" component={MobileIndex}></Route>
+            <Route path="/details/:uniquekey" component={MobileNewsDetails}></Route>
+          </Router>
+
         </MediaQuery>
       </div>
     );
