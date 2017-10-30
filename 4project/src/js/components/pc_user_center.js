ecce4310fcbb8856653c3f2370f60070e67159c3
zhangtanlin90@126.com <ztl900211>
2017-10-30 13:49:41 +0800

    个人中心-我的收藏和我的评论模块添加

--- 4project/src/js/components/pc_user_center.js
+++ 4project/src/js/components/pc_user_center.js
@@ -19,27 +19,54 @@ export default class PCUserCenter extends React.Component {
     super();
     //定义上传按钮
     this.state = {
+      usercollection:"",     //我的收藏的数据【需要的state】
+      usercomment:"",        //我的评论列表数据【需要的state】
       previewImage:"",       //设置预览图为空
       previewVisible : false //默认隐藏模态框
     };
   }
 
+  //页面数据加载完成后，调用
+  componentDidMount(){
 
-  handleCancel(){
-    this.setState({
-      previewVisible: true
-    })
+    //获取收藏列表,并把值赋给state
+    var myFetchOptions = {
+      method:"GET"
+    }
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid="+localStorage.userid,
+      myFetchOptions)
+      .then(response => response.json())
+      .then(json => {
+        //注意setState()调用方法和this.state方法
+        this.setState({
+          usercollection:json
+        });
+      });
+
+    //获取评论列表,并把值赋给state
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid="+localStorage.userid,
+      myFetchOptions)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          usercomment:json
+        })
+      });
+
   }
 
   render() {
 
+    //上传头像的props定义【antd方法自带】
     const props = {
+      //上传地址【地址需要后台提供】
       action:"//oygy32qzs.bkt.clouddn.com/reacttest",
+      //响应头
       headers:{
         "Access-Control-Allow-Origin":"*"
       },
       listType:"picture-card",
-      //设置antd自带方法，默认图片显示
+      //设置默认图片显示【antd自带方法】
       defaultFileList:[
         {
           uid:-1,
@@ -58,6 +85,38 @@ export default class PCUserCenter extends React.Component {
       }
     }
 
+    /*我的收藏列表定义展示
+      注意:{usercollection}等价于const usercollection = this.state.usercollection
+    */
+    const {usercollection} = this.state;
+    const usercollectionList = usercollection.length
+      ?
+        usercollection.map((collection,index) => (
+          /*用Card模块展示我的收藏数据
+          * 注意extra的href链接调用方法
+          * 当链接里面带参数时的固定写法用``包含
+          * */
+          <Card key={index} title={collection.uniquekey} extra={<a target="_blank" href={`/#/details/${collection.uniquekey}`}>查看所在新闻</a>}>
+            <p>{collection.Title}</p>
+          </Card>
+        ))
+      :
+        "您还没有收藏新闻"
+      ;
+
+    //我的评论列表数据展示
+    const {usercomment} = this.state;
+    const usercommentList = usercomment.length
+      ?
+      usercomment.map((comment,index) => (
+          <Card key={index} title={`您于 ${comment.datetime} 评论了文章 ${comment.uniquekey}`} extra={<a href={`/#/details/${comment.uniquekey}`}>跳转被评论的文章</a>}>
+            <p>{comment.Comments}</p>
+          </Card>
+        ))
+      :
+        "暂无评论，快去评论下吧"
+      ;
+
     return (
       <div>
         <PCHeader/>
@@ -65,8 +124,26 @@ export default class PCUserCenter extends React.Component {
           <Col span={2}></Col>
           <Col span={20}>
             <Tabs>
-              <TabPane tab="我的收藏" key="1"></TabPane>
-              <TabPane tab="我的评论" key="2"></TabPane>
+              <TabPane tab="我的收藏" key="1">
+
+                {/*我的收藏列表展示*/}
+                <Row>
+                  <Col span={24}>
+                    {usercollectionList}
+                  </Col>
+                </Row>
+
+              </TabPane>
+              <TabPane tab="我的评论" key="2">
+
+                {/*展示我的评论模块信息*/}
+                <Row>
+                  <Col>
+                    {usercommentList}
+                  </Col>
+                </Row>
+
+              </TabPane>
               <TabPane tab="头像设置" key="3">
 
                 {/*上传控件的调用。{...props}表示获取父组件传递来的值*/}
