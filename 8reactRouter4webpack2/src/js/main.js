ffc921ecb0d60ca8d4cdd9f24944fbc27c5862c2
zhangtanlin90@126.com <ztl900211>
2017-11-16 19:39:27 +0800

    重写webpack

--- 8reactRouter4webpack2/src/js/main.js
+++
@@ -1,8 +0,0 @@
-function alertNumber(number){
-  alert(number);
-}
-function alertString(string){
-  alert(string);
-}
-alertString("我不爱美女");
-alertNumber(123456798);
--- 8reactRouter4webpack2/webpack.config.js
+++ 8reactRouter4webpack2/webpack.config.js
@@ -1,23 +1,49 @@
 const wevpack = require('webpack');
 const path = require('path');
-
-const htmlWebpackPlugin = require('html-webpack-plugin');
+//处理html
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//处理css
 
 module.exports = {
-  extry:{
-    alert_number: './src/js/alert_number.js',
-    alert_string: './src/js/alert_string.js',
-    all:['./src/js/alert_number.js','./src/js/alert_string.js']
+  entry:{
+    all:
+      [
+        './src/css/public.css',
+        './src/css/li.css',
+        './src/css/p.css',
+        './src/css/table.css'
+      ],
+    all:
+      [
+        './src/js/alert_number.js',
+        './src/js/alert_string.js'
+      ]
   },
   output:{
     path: path.resolve(__dirname,'./dist'),
     filename: 'js/[name].js'
 
   },
+  devServer:{
+    port:8080
+  },
   plugins: [
-    new htmlWebpackPlugin({
-      template:'index.html',
-      filename:'index-[hash].html'
+    new HtmlWebpackPlugin({
+      title:'测试统一title',
+      template:'./src/index.html',
+      filename:'index-[hash].html',
+      /*
+      *inject的取值默认true
+      * true或者body：所有JavaScript资源插入到body元素的底部
+      * head: 所有JavaScript资源插入到head元素中
+      * false： 所有静态资源css和JavaScript都不会注入到模板文件中
+      * */
+      inject:'body',
+      /*
+      *hash的取值默认false
+      * 是否为所有注入的静态资源添加webpack每次编译产生的唯一hash值
+      * */
+      hash:false
     })
   ]
 }
